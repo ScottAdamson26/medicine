90bd3f8630daedd07fabbbb6052ca62554b11525
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2024-04-20 11:11:14 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: 'C:/Users/scott/Documents/med/.env'});
+require('dotenv').config({path: '../env'});
 const express = require('express');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,6 @@
 // Import the functions you need from the SDKs you need
+require('dotenv').config({path: '../env'});
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
